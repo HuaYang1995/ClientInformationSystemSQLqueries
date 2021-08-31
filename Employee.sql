4d68d38e6e67f5d22af4614ec8f09b0c014dc35f
HuaYang1995 <87402971+HuaYang1995@users.noreply.github.com>
2021-08-31 14:43:28 -0400

    Add files via upload

---
+++ Employee.sql
@@ -0,0 +1,10 @@
+Use ClientInformationSystem
+
+insert into Employees values('James Harden','asdqee1','Point Guard')
+insert into Employees values('Kobe Bryant','as4as1','Shooting Guard')
+insert into Employees values('Steve Nash','as5s111','Point Guard')
+insert into Employees values('Jason Kidd','21fas1','Point Guard')
+insert into Employees values('Chris Paul','fqfas1','Point Guard')
+insert into Employees values('Kevin Durant','asas1','Small Forward')
+insert into Employees values('Goran Dragic','77fas1','Point Guard')
+insert into Employees values('Luka Doncic','421as1','Point Guard')
