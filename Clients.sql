46e104bda30f93870c10d4176a518e35dd653edb
HuaYang1995 <87402971+HuaYang1995@users.noreply.github.com>
2021-08-31 14:41:34 -0400

    Add files via upload

---
+++ Clients.sql
@@ -0,0 +1,11 @@
+use ClientInformationSystem;
+
+insert into Clients values('Christiano Ronaldo','Cr777@qq.com','13225486','Manchester','2021-08-30 13:27:27')
+insert into Clients values('Leon Messi','LM101010@qq.com','123486231','Paris','2021-08-14 10:10:10')
+insert into Clients values('Cesc Fabregas','CF4444@qq.com','35137321','London','2010-04-13 04:04:04')
+insert into Clients values('Van Der Sar' ,'VSD1111@qq.com','21354895','London','2011-03-22 01:01:01')
+insert into Clients values('Wesley Sneijder','WS1010@qq.com','20132332','Milan','2021-02-23 03:03:23')
+insert into Clients values('Luka Modric','LM1010@qq.com','1231231','Madrid','2012-11-23 13:23:33')
+insert into Clients values('Gareth Bale','GB0909@qq.com','1039215415','London','2010-03-22 04:04:04')
+insert into Clients values('Christian Pulisic' ,'CP1010@qq.com','12315131','London','2015-11-13 15:12:12')
+
