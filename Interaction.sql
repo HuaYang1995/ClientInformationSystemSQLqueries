ba54121b5979aa09f35b3e66818c0fd294153d43
HuaYang1995 <87402971+HuaYang1995@users.noreply.github.com>
2021-08-31 14:42:39 -0400

    Add files via upload

---
+++ Interaction.sql
@@ -0,0 +1,16 @@
+Insert into Interactions values(1,1,'S','2021-09-12 11:12:13','Enjoy the Game!')
+Insert into Interactions values(2,2,'T','2021-09-12 11:12:13','Enjoy the Game!')
+Insert into Interactions values(3,1,'W','2021-09-12 11:12:13','Enjoy the Game!')
+Insert into Interactions values(4,3,'S','2021-09-12 11:12:13','Enjoy the Game!')
+Insert into Interactions values(5,4,'I','2021-09-12 11:12:13','Enjoy the Game!')
+Insert into Interactions values(6,5,'S','2021-09-12 11:12:13','Enjoy the Game!')
+Insert into Interactions values(7,6,'W','2021-09-12 11:12:13','Enjoy the Game!')
+Insert into Interactions values(1,8,'T','2021-09-12 11:12:13','Enjoy the Game!')
+Insert into Interactions values(8,7,'S','2021-09-12 11:12:13','Enjoy the Game!')
+Insert into Interactions values(2,3,'I','2021-09-12 11:12:13','Enjoy the Game!')
+
+
+
+;
+
+
